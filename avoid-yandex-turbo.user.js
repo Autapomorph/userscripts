09c9af108217279f5bf12cfe04c2d6fab42c202c
Autapomorph <Autapomorph@users.noreply.github.com>
2020-11-19 16:39:59 +0300

    chore(avoid-yandex-turbo): release 2.0.0

--- avoid-yandex-turbo.user.js
+++ avoid-yandex-turbo.user.js
@@ -4,7 +4,7 @@
 // @description Redirect directly to target page avoiding Yandex Turbo
 // @description:ru Переадресация на целевую страницу в обход Яндекс Турбо
 // @author Autapomorph
-// @version 1.0.13
+// @version 2.0.0
 // @downloadURL https://github.com/Autapomorph/userscripts/raw/main/avoid-yandex-turbo.user.js
 // @updateURL https://github.com/Autapomorph/userscripts/raw/main/avoid-yandex-turbo.user.js
 // @run-at document_start
@@ -15,118 +15,81 @@
 // ==/UserScript==
 
 (function () {
-  function getUrlVar() {
-    var urlVar = window.location.search;
-    var arrayVar = [];
-    var valueAndKey = [];
-    var resultArray = [];
-    arrayVar = urlVar.substr(1).split('&');
-
-    if (arrayVar[0] === '') {
-      return false;
+  function redirectWithTurboOverlay() {
+    var titleHostActive = $('.turbo-overlay__title-host_active');
+    if (!titleHostActive || !titleHostActive.length) {
+      return;
     }
 
-    for (i = 0; i < arrayVar.length; i++) {
-      valueAndKey = arrayVar[i].split('=');
-      resultArray[valueAndKey[0]] = valueAndKey[1];
+    
+    var titleHostActiveText = titleHostActive[0].textContent;
+    if (!titleHostActiveText.length) {
+      return;
     }
 
-    return resultArray;
-  }
-
-  var urlLandingPage = getUrlVar();
-  var urlHostname = window.location.hostname;
-  var urlPathname = window.location.pathname;
-
-  // turbopages.org
-  if (/.+.turbopages.org$/.test(urlHostname)) {
-    if (/\.*\/(s|h)\/.*/.test(urlPathname)) {
-      var turboIndex = urlPathname.indexOf('/turbo/');
-      var delimeterIndex = urlPathname.search(/\/(s|h)/);
-      var delimeterLength = 2;
-
-      if (delimeterIndex < 0) return;
+    var hostLinks = $('a[data-sc-host]');
+    for (var i = 0; i < hostLinks.length; i += 1) {
+      var hostLink = $(hostLinks[i]);
       
-      var host =
-        turboIndex === -1
-          ? urlPathname.substring(1, delimeterIndex)
-          : urlPathname.substring(turboIndex + '/turbo/'.length, delimeterIndex);
-      var pathName = urlPathname.substring(delimeterIndex + delimeterLength);
-      top.location.replace('//' + host + pathName);
+      var dataCounter;
+      try {
+        dataCounter = JSON.parse(hostLink.attr('data-counter'));
+      } catch (error) {
+        return;
+      }
+
+      if (dataCounter.find(e => e.indexOf(titleHostActiveText) > -1)) {
+        var redirect;
+        if (dataCounter[0] === 'b') {
+          redirect = dataCounter[1];
+        } else if (dataCounter[0] === 'w') {
+          redirect = dataCounter[3];
+        } else {
+          return;
+        }
+        
+        top.location.replace(redirect);
+      }
     }
 
-    return;
+    $('a[data-sc-host]').each(function () {
+      var dataCounterAttr = $(this).attr('data-counter');
+      if (!dataCounterAttr) return;
+
+      var dataCounter = JSON.parse(dataCounterAttr);
+      if (dataCounter.find(e => e.indexOf(titleHostActiveText) > -1)) {
+        var redirect;
+        if (dataCounter[0] === 'b') {
+          redirect = dataCounter[1];
+        } else if (dataCounter[0] === 'w') {
+          redirect = dataCounter[3];
+        }
+        top.location.replace(redirect);
+      }
+    });
   }
 
-  // yandex.ru
-  if (urlPathname === '/turbo') {
-    top.location.replace(decodeURIComponent(urlLandingPage['text']));
-  } else if (/\/turbo\/.*\/(s|h)\/.*/.test(urlPathname)) {
+  function redirectWithURL() {
+    var urlPathname = top.location.pathname;
     var turboIndex = urlPathname.indexOf('/turbo/');
     var delimeterIndex = urlPathname.search(/\/(s|h)/);
     var delimeterLength = 2;
 
-    if (delimeterIndex < 0) return;
-
-    var host = urlPathname.substring(turboIndex + '/turbo/'.length, delimeterIndex);
+    if (delimeterIndex < 0) {
+      return;
+    }
+    
+    var host =
+      turboIndex === -1
+        ? urlPathname.substring(1, delimeterIndex)
+        : urlPathname.substring(turboIndex + '/turbo/'.length, delimeterIndex);
     var pathName = urlPathname.substring(delimeterIndex + delimeterLength);
     top.location.replace('//' + host + pathName);
-  } else if (urlPathname.indexOf('/turbo/s/') !== -1) {
-    top.location.replace('https://' + urlPathname.substr(urlPathname.indexOf('/turbo/s/') + 9));
-  } else if (urlPathname === '/search/touch/') {
-    $('a[data-sc-host]').each(function () {
-      var urlYaTurbo = $(this).attr('href');
-      var dataCounter = JSON.parse($(this).attr('data-counter'));
-      if (
-        urlYaTurbo.indexOf('https://yandex.ru/turbo/s/') !== -1 ||
-        urlYaTurbo.indexOf('https://yandex.ru/turbo?text=') !== -1
-      ) {
-        $(this).attr('data-bem', '{"link":{}}');
-        if (dataCounter[0] === 'b') {
-          $(this).attr('href', dataCounter[1]);
-        } else if (dataCounter[0] === 'w') {
-          $(this).attr('href', dataCounter[3]);
-        }
-      }
-    });
-  } else if (urlPathname === '/search/') {
-    $('a.link').each(function () {
-      var dataCounter = JSON.parse($(this).attr('data-counter'));
-      if (dataCounter[0] === 'b') {
-        $(this).attr('href', dataCounter[1]);
-      } else if (dataCounter[0] === 'w') {
-        if (typeof dataCounter[3] !== 'undefined') {
-          $(this).hide();
-        }
-      }
-    });
-  } else if (urlPathname.substr(0, 12) === '/news/story/') {
-    var YTsubtitle = document.querySelector('div.news-story__head > a.news-story__subtitle');
-    if (
-      YTsubtitle.href.substr(0, 26) === 'https://yandex.ru/turbo/s/' ||
-      YTsubtitle.href.substr(0, 26) === 'https://yandex.ru/turbo/h/'
-    ) {
-      YTsubtitle.href =
-        'https://' + YTsubtitle.href.substring(26, YTsubtitle.href.length).split('?')[0];
-    } else {
-      YTsubtitle.href = YTsubtitle.href.split('?')[0];
-    }
+  }
 
-    var YT = document.querySelectorAll('a.news-snippet__url');
-    for (var i = 0; i < YT.length; i++) {
-      if (
-        YT[i].href.substr(0, 26) === 'https://yandex.ru/turbo/s/' ||
-        YT[i].href.substr(0, 26) === 'https://yandex.ru/turbo/h/'
-      ) {
-        YT[i].href = 'https://' + YT[i].href.substring(26, YT[i].href.length).split('?')[0];
-      } else {
-        YT[i].href = YT[i].href.split('?')[0];
-      }
-    }
-
-    var YTicon = document.querySelectorAll('svg.news-snippet__turbo-icon');
-    for (var i = 0; i < YTicon.length; i++) {
-      YTicon[i].style.display = 'none';
-    }
+  var urlPathname = top.location.pathname;
+  if (/\.*\/(s|h)\/.*/.test(urlPathname)) {
+    redirectWithTurboOverlay();
+    redirectWithURL();
   }
 })();
